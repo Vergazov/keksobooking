1629a1b0a984dc8e88392f174f5ad3d62593d614
Вергазов Артём <vergazka@gmail.com>
2024-05-08 15:12:08 +0300

    разбил файлы на модули

--- js/main.js
+++ js/main.js
@@ -1,31 +1,7 @@
-const getRandomIntInclusive = function(min,max){
+import './data.js';
+import {getUpcomingAnnouncements} from './util.js';
 
-  min = Math.round(min);
-  max = Math.round(max);
-
-  if(min < 0){
-    return 'Минимальное значение должно быть больше 0';
-  }
-  if(min > max || min === max){
-    return 'Минимальное значение должно быть меньше максимального';
-  }
-
-  return Math.floor(Math.random() * (max - min + 1) + min);
-}
-
-const getRandomFloatInclusive = function(min,max, numberOfCharacters){
-
-  if(min < 0){
-    return 'Минимальное значение должно быть больше 0';
-  }
-  if(min > max || min === max){
-    return 'Минимальное значение должно быть меньше максимального';
-  }
-  let rand;
-
-  rand = Math.random() * (max - min) + min;
-  return rand.toFixed(numberOfCharacters);
-
-}
-
-console.log(getRandomFloatInclusive(1.5,2,2));
+let vars = document.querySelector('.promo')
+console.log(vars);
+let upcomingAnnouncements = getUpcomingAnnouncements();
+// console.log(upcomingAnnouncements);
---
+++ js/util.js
@@ -0,0 +1,87 @@
+import {TITLES, DESC, TYPES, CHECKIN, CHECKOUT, FEATURES, PHOTOS } from './data.js';
+
+const getRandomIntInclusive = function (min, max) {
+
+  min = Math.round(min);
+  max = Math.round(max);
+
+  if (min < 0) {
+    return 'Минимальное значение должно быть больше либо равно 0';
+  }
+  if (min > max || min === max) {
+    return 'Минимальное значение должно быть меньше максимального';
+  }
+
+  return Math.floor(Math.random() * (max - min + 1) + min);
+}
+
+const getRandomFloatInclusive = function (min, max, numberOfCharacters) {
+
+  if (min < 0) {
+    return 'Минимальное значение должно быть больше 0';
+  }
+  if (min > max || min === max) {
+    return 'Минимальное значение должно быть меньше максимального';
+  }
+  let rand;
+
+  rand = Math.random() * (max - min) + min;
+  return rand.toFixed(numberOfCharacters);
+
+}
+
+
+const getRandomArrayValues = function (array) {
+
+  let maxCountArray = array.length;
+
+  let randomArrayValues = new Array(getRandomIntInclusive(1, maxCountArray)).fill(null).map(() => {
+    return array[getRandomIntInclusive(0, maxCountArray - 1)];
+  });
+
+  return _.uniq(randomArrayValues);
+}
+
+let getCounter = function (counter) {
+  counter += 1;
+  if (counter < 10) {
+    return '0' + counter;
+  } else {
+    return counter;
+  }
+}
+
+
+const getUpcomingAnnouncements = function () {
+  let upcomingAnnouncements = [];
+  for (let i = 0; i < TITLES.length; i++) {
+    upcomingAnnouncements.push({
+
+      author: {
+        avatar: 'img/avatars/user' + getCounter(i) + '.png',
+      },
+      offer: {
+        title: TITLES[i],
+        adress: 'location.' + getRandomFloatInclusive(1, 10, 2) + ', ' + 'location.' + getRandomFloatInclusive(1, 10, 2),
+        price: getRandomIntInclusive(1500, 5000),
+        type: TYPES[getRandomIntInclusive(0, 4)],
+        rooms: getRandomIntInclusive(1, 5),
+        guests: getRandomIntInclusive(1, 8),
+        checkin: CHECKIN[getRandomIntInclusive(0, 2)],
+        checkout: CHECKOUT[getRandomIntInclusive(0, 2)],
+        features: getRandomArrayValues(FEATURES),
+        description: DESC[i],
+        photos: getRandomArrayValues(PHOTOS),
+      },
+      location: {
+        x: getRandomFloatInclusive(35.65000, 35.70000, 5),
+        y: getRandomFloatInclusive(139.70000, 139.80000, 5),
+      }
+    });
+  }
+  return upcomingAnnouncements;
+}
+
+export {
+  getUpcomingAnnouncements,
+}
