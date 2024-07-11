1f07a98db23c64a072ecc50dee2917e2255461ac
Вергазов Артём <vergazka@gmail.com>
2024-07-11 17:43:43 +0300

    поменял структуру проекта, сделал сборку через webpack

--- source/js/map.js
+++ source/js/map.js
@@ -33,7 +33,7 @@ L.tileLayer(
 ).addTo(map);
 
 const mainPinIcon = L.icon({
-  iconUrl: './leaflet/img/main-pin.svg',
+  iconUrl: '.././leaflet/img/main-pin.svg',
   iconSize: [52, 52],
   iconAnchor: [26, 52],
 });
